e262ad1fd621a875d5d0b74744b4197fb0c12104
Marius Ursu <marius-git@outlook.com>
2023-07-20 19:28:10 +0200

    snapshot, as instructed L.51

---
+++ dbtbootcamp/snapshots/scd_raw_listings.sql
@@ -0,0 +1,17 @@
+{% snapshot scd_raw_listings %}
+
+{{
+   config(
+       target_schema='dev',
+       unique_key='id',
+       strategy='timestamp',
+       updated_at='updated_at',
+       invalidate_hard_deletes=True
+   )
+}} -- invalidate_hard_deletes: this way, deletes are monitored too
+--monitor by timestamp, in particular by updated_at
+
+
+select * FROM {{ source('airbnb', 'listings') }}
+
+{% endsnapshot %}
