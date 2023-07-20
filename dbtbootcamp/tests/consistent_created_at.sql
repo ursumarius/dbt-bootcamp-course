0232eb41dc92364d7a5f7bfb0fe7a6fe636dad69
Marius Ursu <marius-git@outlook.com>
2023-07-20 20:04:14 +0200

    singular test, assignment 2 LAB

---
+++ dbtbootcamp/tests/consistent_created_at.sql
@@ -0,0 +1,8 @@
+-- Lab: checks that there is no review date that is submitted before its listing was created: Make sure that every
+-- review_date in fct_reviews is more recent than the associated created_at in dim_listings_cleansed
+
+SELECT * FROM  {{ ref('dim_listings_cleansed') }} lc
+INNER JOIN {{ ref('fct_reviews') }} f
+USING (listing_id)
+WHERE f.review_date < lc.created_at
+
