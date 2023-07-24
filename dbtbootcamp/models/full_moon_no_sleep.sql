1072ebbfc7cbccb56e367b0825699124f99ac25c
Marius Ursu <marius-git@outlook.com>
2023-07-24 18:32:15 +0200

    correction: they are separate models

--- dbtbootcamp/models/full_moon_no_sleep.sql
+++ dbtbootcamp/models/full_moon_no_sleep.sql
@@ -14,14 +14,3 @@ ORDER BY
     is_full_moon,
     review_sentiment;
 
-WITH full_moon_review_sentiments AS (
-SELECT * FROM {{ ref('full_moon_no_sleep') }}
-)
-SELECT
-is_full_moon,
-review_sentiment,
-reviews
-REVIEWS * 100.0 / SUM(REVIEWS) OVER(PARTITION BY IS_FULL_MOON) AS PERCENTAGE_REVIEWS
-FROM
-full_moon_review_sentiments
-
