5b6a4a1a3a93cc8d61887b7f932357bbe769933b
Marius Ursu <marius-git@outlook.com>
2023-07-25 19:32:34 +0200

    add note on derived table functionality - sql nest

--- dbtbootcamp/analyses/full_moon_no_sleep_percentage.sql
+++ dbtbootcamp/analyses/full_moon_no_sleep_percentage.sql
@@ -8,3 +8,16 @@ SELECT
 	REVIEWS * 100.0 / SUM(REVIEWS) OVER(PARTITION BY IS_FULL_MOON) AS PERCENTAGE_REVIEWS
 FROM
     full_moon_review_sentiments
+
+
+
+-- This is how to do it in one run, without an intermediary model:
+
+-- SELECT department_name, AVG(salary) AS avg_salary
+-- FROM (
+--  SELECT e.employee_id, e.salary, d.department_name
+--   FROM employees e
+--   INNER JOIN departments d ON e.department_id = d.department_id
+-- ) AS derived_table
+-- GROUP BY department_name
+-- HAVING COUNT(employee_id) >= 10;
