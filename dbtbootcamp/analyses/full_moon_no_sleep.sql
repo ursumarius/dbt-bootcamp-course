6a2c774f5b37d53dbf1d5f6a278f7f0f51de55aa
Marius Ursu <marius-git@outlook.com>
2023-07-28 13:43:18 +0200

    cleaning up directories: tests and analyses

---
+++ dbtbootcamp/analyses/full_moon_no_sleep.sql
@@ -0,0 +1,18 @@
+-- Gets the data ready for a visualization of how fullmoon dates correlate with less than positive reviews
+
+
+WITH mart_fullmoon_reviews AS (
+    SELECT * FROM {{ ref('mart_fullmoon_reviews') }}
+)
+SELECT
+    is_full_moon,
+    review_sentiment,
+    COUNT(*) as reviews
+FROM
+    mart_fullmoon_reviews
+GROUP BY
+    is_full_moon,
+    review_sentiment
+ORDER BY
+    is_full_moon,
+    review_sentiment
--- dbtbootcamp/tests/consistent_created_at.sql
+++ dbtbootcamp/tests/consistent_created_at.sql
@@ -1,9 +1,13 @@
--- Lab: checks that there is no review date that is submitted before its listing was created: Make sure that every
--- review_date in fct_reviews is more recent than the associated created_at in dim_listings_cleansed
+
+{% test consistent_review_created_at(model) %}
+
+-- Lab: checks that there is no review date that is submitted before its listing was created:
+-- Make sure that every review_date in fct_reviews is more recent than the associated created_at in dim_listings_cleansed
 
 SELECT * FROM  {{ ref('dim_listings_cleansed') }} lc
 INNER JOIN {{ ref('fct_reviews') }} f
 USING (listing_id)
 WHERE f.review_date < lc.created_at
 
---testing git merge -ff-only
+{% endtest %}
+
